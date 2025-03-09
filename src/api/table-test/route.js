a2bab5a000cf7f683d510bbb90d480315910ab04
Adama55 <mafahfof55@gmail.com>
2025-03-09 18:36:54 +0100

    "connection etablished"

--- src/api/table-test/route.js
+++ src/api/table-test/route.js
@@ -1,5 +1,17 @@
 import { supabase } from "@/lib/supabase";
 
+
+export async function GET() {
+  const { data, error } = await supabase.from("table-test").select("*");
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
+}
+
+/* 
 export async function GET() {
   try {
     const { data, error } = await supabase.from("products").select("*");
@@ -33,3 +45,4 @@ export async function POST(req) {
     }
   }
   
+*/
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,5 +1,15 @@
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/api/table-test")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Erreur de récupération :", error));
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
@@ -9,12 +19,17 @@ export default function Products() {
         <div>
       <h2>Nos produits</h2>
       <ul>
-        <li>Produit 1</li>
-        <li>Produit 2</li>
-        <li>Produit 3</li>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <strong>{product.price} €</strong>
+          </li>
+        ))}
       </ul>
     </div>
      
     </div>
   );
 }
+
