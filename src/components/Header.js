778b9232ead906cda55ece7a26152f17295a3e88
Adama55 <mafahfof55@gmail.com>
2025-05-17 17:25:29 +0200

    add page home and page products

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,26 @@ import { ShoppingCart, User, Menu, X } from "lucide-react";
 import Image from "next/image";
 import logo from "../logo-bw.png";
 
+
+
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-[#7AD154] text-black p-4 shadow-md">
+    <header className="bg-black text-white p-4 shadow-md"> 
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <h1 className="text-2xl font-bold">
-          <Image src={logo} alt="Mon logo" width={60} height={60} />
+          <Image src={logo} alt="Mon logo" width={70} height={70} />
           
           </h1>
 
         {/* Menu Desktop (toujours visible) */}
         <nav className="hidden md:flex space-x-6">
-          <a href="/" className="hover:underline">Accueil</a>
-          <a href="/boutique" className="hover:underline">Boutique</a>
-          <a href="/about" className="hover:underline">À propos de nous</a>
-          <a href="/profil" className="hover:underline flex items-center">
+          <a href="/" className="hover:underline hover:text-green-300 active:text-green-500 active:underline">Accueil</a>
+          <a href="/products" className="hover:underline hover:text-green-300 active:text-green-500 active:underline">Boutique</a>
+          <a href="/about" className="hover:underline hover:text-green-300 active:text-green-500 active:underline">À propos de nous</a>
+          <a href="/profil" className="hover:underline hover:text-green-300 active:text-green-500 active:underline">
             <User className="w-6 h-6" />
           </a>
           <a href="/panier" className="hover:underline flex items-center">
@@ -40,7 +42,7 @@ export default function Header() {
 
       {/* Menu Mobile (affichage conditionnel) */}
       {isOpen && (
-        <nav className="md:hidden flex flex-col items-center bg-yellow-700 py-4 space-y-2">
+        <nav className="md:hidden flex flex-col items-center bg-black py-4 space-y-2">
           <a href="/" className="hover:underline">Accueil</a>
           <a href="/boutique" className="hover:underline">Boutique</a>
           <a href="/about" className="hover:underline">À propos de nous</a>
